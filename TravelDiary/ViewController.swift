6d47f7e40ddbf96540c1f5bdf52828ab3d2d5e3a
Oleksandr Sofishchenko <insten490@gmail.com>
2016-10-02 02:49:11 -0500

    Final Copy

---
+++ TravelDiary/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  TravelDiary
+//
+//  Created by Oleksandr Sofishchenko on 10/1/16.
+//  Copyright © 2016 Oleksandr Sofishchenko. All rights reserved.
+//
+
+import UIKit
+//import CoreLocation
+import Alamofire
+
+
+
+class ViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //self.headLabel.text = "Welcome to College Station"
+        //self.historyLabel.text = "History"
+        //self.foodLabel.text = "Local Food 🍔"
+        
+        
+    
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
